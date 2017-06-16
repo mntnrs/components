4029b20aabd7644596885faac6c36ee8a7256c57
christopherbiscardi <chris@christopherbiscardi.com>
2017-06-16 08:22:13 -0700

    rm lib from .git

--- packages/constants/lib/colors.js
+++
@@ -1,8 +0,0 @@
-export default {
-  brand: "#f30ba1",
-  success: "#09cd4f", //"#0bf35d",
-  digitalBlue: "#0ba1f3",
-  alert: "#f30b2d",
-  warning: "#f74004"
-};
-//# sourceMappingURL=colors.js.map
