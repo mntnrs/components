ee2fa725d5d0040f8da8de9500f3ecd89631686f
Christopher Biscardi <biscarch@sketcht.com>
2017-06-15 22:25:43 -0700

    system font

---
+++ packages/system-font/index.js
@@ -0,0 +1,8 @@
+/* Define the "system" font family */
+export const system = `@font-face {
+  font-family: system;
+  font-style: normal;
+  font-weight: 300;
+  src: local(".SFNSText-Light"), local(".HelveticaNeueDeskInterface-Light"), local(".LucidaGrandeUI"), local("Ubuntu Light"), local("Segoe UI Light"), local("Roboto-Light"), local("DroidSans"), local("Tahoma");
+}
+`;
